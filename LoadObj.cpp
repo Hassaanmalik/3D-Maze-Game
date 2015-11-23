baa7836175ba23fd1c6df761a0ab2fc23d451f89
Navleen <navleen95@yahoo.ca>
2015-11-22 20:19:35 -0500

    Update
    Full reads the obj file

--- LoadObj.cpp
+++ LoadObj.cpp
@@ -25,16 +25,18 @@ struct Vertice{
 	float z;
 };
 struct Face{
-	int point1;
-	int point2;
-	int point3;
-	int point4;
+	int p1;
+	int p2;
+	int p3;
+	int p4;
 };
 
 vector<Vertice>vertices;
 vector<Vertice>vertices_texture;
 vector<Vertice>vertices_normal;
-vector<Face>face;
+vector<Face>faces;
+vector<Face>faces_texture;
+vector<Face>faces_normal;
 
 
 LoadObj::LoadObj(char *objFile){
@@ -54,20 +56,41 @@ void LoadObj::setVertices(){
 			if(res == EOF){
 				break;
 			}else{
-				if(strcmp(lineHeader, "#") == 0){
-				}else if(strcmp(lineHeader, "v") == 0){
-					//Vertice temp_vertices;
-					//float x, y, z;
-					//fscanf(file, "%f %f %f\n", &x, &y, &z);
-					//temp_vertices.x = x;
-					//temp_vertices.y = y;
-					//temp_vertices.z = z;
+				if(strcmp(lineHeader, "v") == 0){
+					Vertice temp_vertice;
+					fscanf(file, "%f %f %f\n", 
+							&temp_vertice.x, &temp_vertice.y, &temp_vertice.z);
 
-					//printf("%f\n", x);
+					vertices.push_back(temp_vertice);
 				}else if(strcmp(lineHeader, "vn") == 0){
+					Vertice temp_vertice_normal;
+					fscanf(file, "%f %f %f\n", 
+							&temp_vertice_normal.x, &temp_vertice_normal.y, &temp_vertice_normal.z);
+
+					vertices_normal.push_back(temp_vertice_normal);
 				}else if(strcmp(lineHeader, "vt") == 0){
+					Vertice temp_vertice_texture;
+					fscanf(file, "%f %f %f\n", 
+							&temp_vertice_texture.x, &temp_vertice_texture.y);
+
+					vertices_texture.push_back(temp_vertice_texture);
 				}else if(strcmp(lineHeader, "f") == 0){
+					Face temp_face, temp_face_texture, temp_face_normal;
+					fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", 
+							&temp_face.p1, &temp_face.p2, &temp_face.p3, &temp_face.p4,
+							&temp_face_texture.p1, &temp_face_texture.p2, &temp_face_texture.p3, &temp_face_texture.p4,
+							&temp_face_normal.p1, &temp_face_normal.p2, &temp_face_normal.p3, &temp_face_normal.p4);
+
+					faces.push_back(temp_face);
+					faces_texture.push_back(temp_face_texture);
+					faces_normal.push_back(temp_face_normal);
 				}
+				/*else if(strcmp(lineHeader, "#") == 0){
+				}else if(strcmp(lineHeader, "o") == 0){
+				}else if(strcmp(lineHeader, "mtllib") == 0){
+				}else if(strcmp(lineHeader, "usemtl") == 0){
+				}else if(strcmp(lineHeader, "s") == 0){
+				}*/
 			}
 		}
 		fclose(file);
